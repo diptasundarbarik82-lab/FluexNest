95ab1ce6c5fcb061e7b059ea6277355a313c0555
diptasundarbarik82-lab <diptasundarbarik82@gmail.com>
2026-07-09 18:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// FluexNest JavaScript
-
-const button = document.querySelector("button");
-
 button.addEventListener("click", function () {
     alert("Welcome to FluexNest! Shopping feature is coming soon.");
 });
