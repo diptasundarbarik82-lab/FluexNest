c78b9412c2f44ba883278230a582831d1c498088
diptasundarbarik82-lab <diptasundarbarik82@gmail.com>
2026-07-09 18:26:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.querySelector("button").addEventListener("click", function () {
+const shopBtn = document.getElementById("shopBtn");
+
+shopBtn.addEventListener("click", () => {
     document.querySelector(".products").scrollIntoView({
         behavior: "smooth"
     });
