50d73cdb423faf419ca8e1624e0a4cd59496a635
diptasundarbarik82-lab <diptasundarbarik82@gmail.com>
2026-07-12 20:50:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function buyProduct(product) {
         "\nOnline shopping feature will be available soon."
     );
 }
+let cart = 0;
+
+const buttons = document.querySelectorAll(".add-cart");
+const cartText = document.querySelector("#cart-count");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        cart++;
+        cartText.textContent = cart;
+        alert("Product added to cart!");
+    });
+});
